218f0f72a8534f7b37349bdae244751903b3d7c5
zz2go <142401613+zz2go@users.noreply.github.com>
2022-07-31 14:23:04 +0000

    Add patch

--- apis.js
+++ apis.js
@@ -2,3 +2,5 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 import { Configuration, OpenAIApi } from "openai";
+import { writeUserData } from "./database.js";
+import { getHistory, AI_NAME } from "./utils.js";
