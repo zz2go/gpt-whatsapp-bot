23a9dd2b5b025e554883bb647133e6f619c7ddcb
zz2go <142401613+zz2go@users.noreply.github.com>
2022-07-31 22:13:55 +0000

    2.8.4

--- apis.js
+++ apis.js
@@ -4,3 +4,6 @@ dotenv.config();
 import { Configuration, OpenAIApi } from "openai";
 import { writeUserData } from "./database.js";
 import { getHistory, AI_NAME } from "./utils.js";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_KEY,
