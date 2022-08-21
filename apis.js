7ac45fe60a7136050501a93ec8db9c81b00b33ea
zz2go <142401613+zz2go@users.noreply.github.com>
2022-08-21 13:27:08 +0000

    removed console output

--- apis.js
+++ apis.js
@@ -49,3 +49,5 @@ export const getDalleResponse = async (sender_text) => {
   const options = {
     prompt: sender_text, // Image description
     n: 1, // // Number of images to be generated
+    size: "1024x1024", // Image size
+  };
