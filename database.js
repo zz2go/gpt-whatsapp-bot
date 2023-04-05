3ddff0003bce8244bf5f693bb6bfa0cd706e2f09
zz2go <142401613+zz2go@users.noreply.github.com>
2023-04-05 10:05:01 +0000

    [maven-release-plugin] prepare release v1.0.2

--- database.js
+++ database.js
@@ -21,4 +21,4 @@ const firebase_app =
 
 const db = process.env.FIREBASE_DB_URL && getDatabase(firebase_app);
 
-export const writeUserData = async function (sender_id, sender_text, resp
+export const writeUserData = async function (sender_id, sender_text, respon
