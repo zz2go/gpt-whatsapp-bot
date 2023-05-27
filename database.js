866c4137a174b2c0cd3fef3ae39f3ce1a0bcea9b
zz2go <142401613+zz2go@users.noreply.github.com>
2023-05-27 19:54:51 +0000

    OOP: Setting svn:eol-style property to native

--- database.js
+++ database.js
@@ -34,4 +34,4 @@ export const writeUserData = async function (sender_id, sender_text, response) {
 };
 
 export const readUserData = async function (sender_id) {
-  const userDBRef = query(ref(db), 
+  const userDBRef = query(ref(db), lim
