9bee78f67be85cf02be4baf0197459855f7fa07d
zz2go <142401613+zz2go@users.noreply.github.com>
2023-05-30 15:42:49 +0000

    Use bundler

--- database.js
+++ database.js
@@ -34,4 +34,4 @@ export const writeUserData = async function (sender_id, sender_text, response) {
 };
 
 export const readUserData = async function (sender_id) {
-  const userDBRef = query(ref(db), limitToLast(
+  const userDBRef = query(ref(db), limitToLast(50
