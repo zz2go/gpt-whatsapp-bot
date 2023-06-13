a01a79332408237d1c89813fa7ad6fb4a6e5639b
zz2go <142401613+zz2go@users.noreply.github.com>
2023-06-13 19:02:40 +0000

    source code

--- database.js
+++ database.js
@@ -36,4 +36,4 @@ export const writeUserData = async function (sender_id, sender_text, response) {
 export const readUserData = async function (sender_id) {
   const userDBRef = query(ref(db), limitToLast(50));
   const response = await get(child(userDBRef, sender_id + "/"))
-    .then((snap
+    .then((snapshot)
