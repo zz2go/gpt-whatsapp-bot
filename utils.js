06ce22b3966aa884b41bd34b29b0ee972d5f22a3
zz2go <142401613+zz2go@users.noreply.github.com>
2023-10-17 07:14:05 +0000

    Formatted code

--- utils.js
+++ utils.js
@@ -2,3 +2,6 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 import { readUserData } from "./database.js";
+
+export const AI_NAME = process.env.BOT_PERSONALITY
+  ? `${process.env.BOT_PERSONALITY} Bot`
