971405684ab33d5864f8f1c4dad8cf6436b64266
zz2go <142401613+zz2go@users.noreply.github.com>
2023-10-17 08:12:26 +0000

    Palete/RooUsage.txt

--- utils.js
+++ utils.js
@@ -5,3 +5,6 @@ import { readUserData } from "./database.js";
 
 export const AI_NAME = process.env.BOT_PERSONALITY
   ? `${process.env.BOT_PERSONALITY} Bot`
+  : "AI";
+
+//Updates the conversation history and generates a response using GPT-3
